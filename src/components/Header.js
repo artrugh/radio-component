9a5da97506d1058cf6e205fc250eaa8845310f78
artrugh <arturo.rugh@gmail.com>
2020-02-10 23:32:16 +0100

    poweroff event

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,14 +4,15 @@ import uuid from 'uuid';
 
 const Header = props => {
     return (
-        <div  key={uuid.v4()} className="header">
-            <i key={uuid.v4()} className="fas fa-chevron-left"></i>
-            <h2  key={uuid.v4()}
-                className="station"
-                onClick={() => props.onClickStations()}>
+        <div key={uuid.v4()} className="header">
+            <i key={uuid.v4()}
+                className="fas fa-chevron-left" ></i>
+            <h2 key={uuid.v4()}
+                className="station" >
                 STATIONS
                 </h2>
-            <i key={uuid.v4()} className="fas fa-power-off"></i>
+            <i key={uuid.v4()} className="fas fa-power-off"
+                onClick={() => props.onClickStations()}></i>
         </div>
     );
 }
